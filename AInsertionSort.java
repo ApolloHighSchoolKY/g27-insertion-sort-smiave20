469d2a266c2aaa8f5628f70ccd4fee6247272431
smiave20 <avery.smith@stu.daviess.kyschools.us>
2026-03-12 20:02:03 +0000

    Commit

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -1,9 +1,40 @@
+import java.utils.Arrays;
+
 public class AInsertionSort
 {
 
     public static void main(String[] args)
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
+        int temp = 0; 
 
+        //(i) is going through the Array.
+        for(int i = 0; i < myNumbers.length; i++)
+        {
+            //Postion of (x) must be less then the position of the Array.
+            //So it stays in the Array. 
+            for(int x = 0; x < i; x++)
+            {
+                //If the postion at (i) in the Array, is greater then the position of (x) in the Array.
+                if(myNumbers[i] < myNumbers[x])
+                {
+                    //Temp is becoming the postion of (i) in the Array.
+                    temp = myNumbers[i];
+
+                    //(y) equals the position of (i) in the Array.
+                    //But (y) must be greater then the position of (x) in the Array.
+                    for(int y = i; y > x; y--)
+                    {
+                        //Position of (y) in the Array will become the next position in the Array.
+                        myNumbers[y] = myNumbers[y - 1];
+                    }
+                    //Position of (x) will equal the variable (temp) which is the original term/position (i).
+                    myNumbers[x] = temp;
+                }
+            }
+        }
+        
+        //Print out the Array.
+        System.out.println(Arrays.toString(myNumbers));
     }
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -6,29 +6,50 @@ public class ALInsertionSort
     public static void main(String[] args)
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
-    	ArrayList<Integer> sorted = new ArrayList<Integer>();
-    	boolean inserted=false;
 
-		//Add the first number to the array list
+    	ArrayList<Integer> sorted = new ArrayList<Integer>();
+    	
+		boolean inserted = false;
+
+		//Add the first number from the Array to the array list
+		sorted.add(myNumbers[0]);
 
 		//Loop once for all of the remaining numbers in the unsorted list
+		for(int i = 1; i < muNumbers.length; i++)
+		{
+			inserted = false;
 
 			//Check with each of the numbers in the sorted list
-
-				//If this number is less than one in the sorted list,
-				//insert it there
+			//size() for an ArrayList is the whole length.
+			for(int x = 0; x < sorted.size() && !inserted; x++)
+			{
+				//If this number is less than one in the sorted list, insert it there
+				if(myNumbers[i] < sorted.get(x))
+				{
+					//Add, at the postion of x, the term at position in the Array.
+					sorted.add(x, myNumbers[i]);
+					inserted = true;
+				}
+			}
 
 			//If it was not inserted, stick it on the end.
-
-
+			if(!inserted)
+			{
+				//add() inserts the term at the end of the Array.
+				sorted.add(myNumbers[i]);
+			}
+		}
 		//End Loop for unsorted list
 
-		System.out.println(sorted);
-
 		//Move the data back to the array
+		for(int i = 0; i < sorted.size(); i++)
+		{
+			//Within the Array, it will become the postion of (i). 
+			myNumber[i] = sorted.get(i);
+		}
 
 		//Print the contents of the array
-
+		System.out.println(sorted);
     }
 
 
